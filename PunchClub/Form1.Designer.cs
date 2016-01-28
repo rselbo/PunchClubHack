7f3b5ae26dd9919f2f17c35c67f95b9031ef6f55
Roar Selbo <roar@selbo.net>
2016-01-28 13:43:41 +0100

    First commit. Status: Works for me :)

---
+++ PunchClub/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace PunchClub
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.save_1 = new System.Windows.Forms.RadioButton();
+      this.save_2 = new System.Windows.Forms.RadioButton();
+      this.save_3 = new System.Windows.Forms.RadioButton();
+      this.label1 = new System.Windows.Forms.Label();
+      this.money = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.sp = new System.Windows.Forms.TextBox();
+      this.strength = new System.Windows.Forms.TextBox();
+      this.agility = new System.Windows.Forms.TextBox();
+      this.stamina = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.hack = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // save_1
+      // 
+      this.save_1.AutoSize = true;
+      this.save_1.Location = new System.Drawing.Point(12, 12);
+      this.save_1.Name = "save_1";
+      this.save_1.Size = new System.Drawing.Size(60, 17);
+      this.save_1.TabIndex = 0;
+      this.save_1.TabStop = true;
+      this.save_1.Text = "save_1";
+      this.save_1.UseVisualStyleBackColor = true;
+      // 
+      // save_2
+      // 
+      this.save_2.AutoSize = true;
+      this.save_2.Location = new System.Drawing.Point(78, 12);
+      this.save_2.Name = "save_2";
+      this.save_2.Size = new System.Drawing.Size(60, 17);
+      this.save_2.TabIndex = 1;
+      this.save_2.TabStop = true;
+      this.save_2.Text = "save_2";
+      this.save_2.UseVisualStyleBackColor = true;
+      // 
+      // save_3
+      // 
+      this.save_3.AutoSize = true;
+      this.save_3.Location = new System.Drawing.Point(144, 12);
+      this.save_3.Name = "save_3";
+      this.save_3.Size = new System.Drawing.Size(60, 17);
+      this.save_3.TabIndex = 2;
+      this.save_3.TabStop = true;
+      this.save_3.Text = "save_3";
+      this.save_3.UseVisualStyleBackColor = true;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(9, 38);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(39, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Money";
+      // 
+      // money
+      // 
+      this.money.Location = new System.Drawing.Point(73, 35);
+      this.money.Name = "money";
+      this.money.Size = new System.Drawing.Size(199, 20);
+      this.money.TabIndex = 4;
+      this.money.Text = "9999";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(9, 65);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(55, 13);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "SkillPoints";
+      // 
+      // sp
+      // 
+      this.sp.Location = new System.Drawing.Point(73, 62);
+      this.sp.Name = "sp";
+      this.sp.Size = new System.Drawing.Size(199, 20);
+      this.sp.TabIndex = 6;
+      this.sp.Text = "2331";
+      // 
+      // strength
+      // 
+      this.strength.Location = new System.Drawing.Point(73, 88);
+      this.strength.Name = "strength";
+      this.strength.Size = new System.Drawing.Size(199, 20);
+      this.strength.TabIndex = 7;
+      this.strength.Text = "24000";
+      // 
+      // agility
+      // 
+      this.agility.Location = new System.Drawing.Point(73, 114);
+      this.agility.Name = "agility";
+      this.agility.Size = new System.Drawing.Size(199, 20);
+      this.agility.TabIndex = 8;
+      this.agility.Text = "24000";
+      // 
+      // stamina
+      // 
+      this.stamina.Location = new System.Drawing.Point(73, 140);
+      this.stamina.Name = "stamina";
+      this.stamina.Size = new System.Drawing.Size(199, 20);
+      this.stamina.TabIndex = 9;
+      this.stamina.Text = "24000";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(9, 91);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(47, 13);
+      this.label3.TabIndex = 10;
+      this.label3.Text = "Strength";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(9, 117);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(34, 13);
+      this.label4.TabIndex = 11;
+      this.label4.Text = "Agility";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(9, 143);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(45, 13);
+      this.label5.TabIndex = 12;
+      this.label5.Text = "Stamina";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(9, 167);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(276, 78);
+      this.label6.TabIndex = 13;
+      this.label6.Text = resources.GetString("label6.Text");
+      // 
+      // hack
+      // 
+      this.hack.Location = new System.Drawing.Point(12, 252);
+      this.hack.Name = "hack";
+      this.hack.Size = new System.Drawing.Size(260, 23);
+      this.hack.TabIndex = 14;
+      this.hack.Text = "Hack Savegame";
+      this.hack.UseVisualStyleBackColor = true;
+      this.hack.Click += new System.EventHandler(this.hack_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 287);
+      this.Controls.Add(this.hack);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.stamina);
+      this.Controls.Add(this.agility);
+      this.Controls.Add(this.strength);
+      this.Controls.Add(this.sp);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.money);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.save_3);
+      this.Controls.Add(this.save_2);
+      this.Controls.Add(this.save_1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Name = "Form1";
+      this.Text = "PunchClub Save Editor";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.RadioButton save_1;
+    private System.Windows.Forms.RadioButton save_2;
+    private System.Windows.Forms.RadioButton save_3;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox money;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox sp;
+    private System.Windows.Forms.TextBox strength;
+    private System.Windows.Forms.TextBox agility;
+    private System.Windows.Forms.TextBox stamina;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Button hack;
+  }
+}
+
---
+++ PunchClub/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace PunchClub
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private static byte[] key = new byte[32] { 0x7b, 0xd9, 0x4f, 0x0b, 0x18, 0x02, 0x55, 0x2d, 0x72, 0xb8, 0x1b, 0x70, 0x25, 0x70, 0xde, 0xd1, 0xf1, 0x18, 0xaf, 0x90, 0xad, 0x35, 0xc4, 0x13, 0x18, 0x1a, 0x11, 0xda, 0x83, 0xec, 0x35, 0xd1 };
+    private static byte[] vector = new byte[16] { 0x92, 0x40, 0xab, 0xa1, 0x02, 0x03, 0x71, 0x77, 0xe7, 0x79, 0xdd, 0x70, 0x4f, 0x20, 0x72, 0x10 };
+
+    private void hack_Click(object sender, EventArgs e)
+    {
+      char[] quote = new char[1] { '\"' };
+      string strMoney = money.Text.Trim(quote);
+      string strSP = sp.Text.Trim(quote);
+      string strStrength = strength.Text.Trim(quote);
+      string strAgility = agility.Text.Trim(quote);
+      string strStamina = stamina.Text.Trim(quote);
+
+      int iMoney = 0;
+      int iSP = 0;
+      int iStrength = 0;
+      int iAgility = 0;
+      int iStamina = 0;
+
+      if (!Int32.TryParse(strMoney, out iMoney))
+      {
+        MessageBox.Show(this, string.Format("The value \"{0}\" in Money is not a valid number.\nAborting hack!", strMoney));
+        return;
+      }
+      if (!Int32.TryParse(strSP, out iSP))
+      {
+        MessageBox.Show(this, string.Format("The value \"{0}\" in SkillPoints is not a valid number.\nAborting hack!", strSP));
+        return;
+      }
+      if (!Int32.TryParse(strStrength, out iStrength))
+      {
+        MessageBox.Show(this, string.Format("The value \"{0}\" in Strength is not a valid number.\nAborting hack!", strStrength));
+        return;
+      }
+      if (!Int32.TryParse(strAgility, out iAgility))
+      {
+        MessageBox.Show(this, string.Format("The value \"{0}\" in Agility is not a valid number.\nAborting hack!", strAgility));
+        return;
+      }
+      if (!Int32.TryParse(strStamina, out iStamina))
+      {
+        MessageBox.Show(this, string.Format("The value \"{0}\" in Stamina is not a valid number.\nAborting hack!", strStamina));
+        return;
+      }
+
+      int saveGame = 1;
+      if (save_1.Checked) saveGame = 1;
+      if (save_2.Checked) saveGame = 2;
+      if (save_3.Checked) saveGame = 3;
+
+      string dataDir = string.Format("{0}Low\\Lazy Bear Games\\Punch Club", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+      if(Directory.Exists(dataDir))
+      {
+        Directory.SetCurrentDirectory(dataDir);
+      }
+      else
+      {
+        dataDir = Directory.GetCurrentDirectory();
+      }
+
+      string saveName = string.Format("save_{0}.dat", saveGame);
+      if(!File.Exists(saveName))
+      {
+        MessageBox.Show(this, string.Format("Savegame {0} was not found in {1}", saveGame, dataDir));
+      }
+
+      RijndaelManaged rijndaelManaged = new RijndaelManaged();
+      ICryptoTransform encryptor = rijndaelManaged.CreateEncryptor(key, vector);
+      ICryptoTransform decryptor = rijndaelManaged.CreateDecryptor(key, vector);
+
+      string basefilename = string.Format("{0}.old", saveName);
+      int count = 1;
+      string filename = String.Format("{0}{1}", basefilename, count);
+      while (File.Exists(filename)) filename = String.Format("{0}{1}", basefilename, ++count);
+
+      byte[] rawData = File.ReadAllBytes(saveName);
+
+      MemoryStream memoryStream = new MemoryStream();
+      CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Write);
+      cryptoStream.Write(rawData, 0, rawData.Length);
+      cryptoStream.FlushFinalBlock();
+      byte[] data = memoryStream.ToArray();
+      string strData = Encoding.Unicode.GetString(data);
+      int t = strData.Length;
+
+      //save the decrypted file - Mostly for debugging
+      //File.WriteAllBytes(string.Format("{0}.dec", saveName), data);
+
+      List<string> keys = new List<string>();
+      List<string> values = new List<string>();
+
+      //find the correct area
+      int chIndex = FindIndex(data, 8, Encoding.Unicode.GetBytes("_char_res\\\":")); //Key in the ch dictionary
+      ReadJsonList(data, chIndex, keys);
+      if (iMoney >= 0) data = ReplaceValue(data, keys, chIndex, "_money", strMoney);
+      if (iSP >= 0) data = ReplaceValue(data, keys, chIndex, "_sp", strSP);
+      if (iStrength >= 0) data = ReplaceValue(data, keys, chIndex, "_str", strStrength);
+      if (iAgility >= 0) data = ReplaceValue(data, keys, chIndex, "_agl", strAgility);
+      if (iStamina >= 0) data = ReplaceValue(data, keys, chIndex, "_stm", strStamina);
+
+      MemoryStream ememoryStream = new MemoryStream();
+      CryptoStream ecryptoStream = new CryptoStream((Stream)ememoryStream, encryptor, CryptoStreamMode.Write);
+      ecryptoStream.Write(data, 0, data.Length);
+      ecryptoStream.FlushFinalBlock();
+      byte[] edata = ememoryStream.ToArray();
+
+      //save the updated decrypted file - Mostly for debugging
+      //File.WriteAllBytes(string.Format("{0}.upd", saveName), data);
+      File.Move(saveName, filename);
+      File.WriteAllBytes(saveName, edata);
+
+    }
+
+
+    static int GetKeyIndex(byte[] data, int index)
+    {
+      return FindIndex(data, index, Encoding.Unicode.GetBytes("_res_type"));
+    }
+    static int GetValueIndex(byte[] data, int index)
+    {
+      return FindIndex(data, index, Encoding.Unicode.GetBytes("_res_v"));
+    }
+
+    static int FindIndex(byte[] data, int startIndex, byte[] search)
+    {
+      for (int i = startIndex; i < data.Length;)
+      {
+        int tmpI = i;
+        for (int j = 0; j < search.Length; ++j) //start at 8 to skip the string header
+        {
+          byte a = data.ElementAt(tmpI);
+          byte b = search.ElementAt(j);
+          if (a == b)
+          {
+            ++tmpI;
+          }
+          else
+          {
+            i += j + 1; //skip the bytes searched
+            break;
+          }
+
+          if (j == search.Length - 1) //full match found
+          {
+            return i;
+          }
+        }
+      }
+      return -1; // no match found
+    }
+    static void ReadJsonList(byte[] data, int chIndex, List<string> list)
+    {
+      byte[] comma = Encoding.Unicode.GetBytes(",");
+      char[] trim = new char[4] { '\\', '\"', '[', ']' };
+      int startIndex = GetKeyIndex(data, chIndex);
+
+      int start = FindIndex(data, startIndex, Encoding.Unicode.GetBytes("["));
+      int end = FindIndex(data, startIndex, Encoding.Unicode.GetBytes("]"));
+      for (int i = start; i < end;)
+      {
+        int endword = FindIndex(data, i, comma);
+        string word = Encoding.Unicode.GetString(data, i, endword - i);
+        word = word.Trim(trim);
+        list.Add(word);
+        i = endword + 2;
+      }
+    }
+
+    static byte[] ReplaceValue(byte[] data, List<string> keys, int chIndex, string key, string newValue)
+    {
+      byte[] comma = Encoding.Unicode.GetBytes(",");
+      byte[] endbracket = Encoding.Unicode.GetBytes("]");
+      int valueIndex = GetValueIndex(data, chIndex);
+
+      int index = keys.IndexOf(key);
+      if (index == -1)
+      {
+        byte[] insertKey = Encoding.Unicode.GetBytes(string.Format(",\\\\\\\"{0}\\\\\\\"", key));
+        int keyIndex = GetKeyIndex(data, chIndex);
+        int endKeyIndex = FindIndex(data, keyIndex, endbracket);
+        byte[] insertValue = Encoding.Unicode.GetBytes(string.Format(",{0}", newValue));
+        int endValueIndex = FindIndex(data, valueIndex, endbracket);
+
+        byte[] updatedData = new byte[data.Length + insertKey.Length + insertKey.Length];
+        int updateDataIndex = 0;
+        Buffer.BlockCopy(data, 0, updatedData, updateDataIndex, endKeyIndex); //copy data up to the end of the keys 
+        updateDataIndex += endKeyIndex;
+        Buffer.BlockCopy(insertKey, 0, updatedData, updateDataIndex, insertKey.Length);//copy in the new key
+        updateDataIndex += insertKey.Length;
+        Buffer.BlockCopy(data, endKeyIndex, updatedData, updateDataIndex, endValueIndex - endKeyIndex);//copy the data from the old end to the new value start
+        updateDataIndex += endValueIndex - endKeyIndex;
+        Buffer.BlockCopy(insertValue, 0, updatedData, updateDataIndex, insertValue.Length); //copy in the new value
+        updateDataIndex += insertValue.Length;
+        Buffer.BlockCopy(data, endValueIndex, updatedData, updateDataIndex, data.Length - endValueIndex);//copy the rest of the data
+
+        return updatedData;
+      }
+      else
+      {
+        for (int i = 0; i < index; ++i) //skip to the correct column
+        {
+          valueIndex = FindIndex(data, valueIndex, comma) + 2; //+2 to skip the unicode ,
+        }
+        int endIndex = FindIndex(data, valueIndex, comma); //find the end of the value
+        int len = endIndex - valueIndex;
+        byte[] value = Encoding.Unicode.GetBytes(newValue);
+        byte[] updatedData = new byte[data.Length + value.Length - len];
+        Buffer.BlockCopy(data, 0, updatedData, 0, valueIndex);
+        Buffer.BlockCopy(value, 0, updatedData, valueIndex, value.Length);
+        Buffer.BlockCopy(data, endIndex, updatedData, valueIndex + value.Length, data.Length - endIndex);
+        return updatedData;
+      }
+    }
+  }
+}
